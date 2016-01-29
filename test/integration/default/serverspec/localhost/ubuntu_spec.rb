92a2641cdfb9516a6932ba7d33584321f2457060
munjeli <elemunjeli@gmail.com>
2016-01-28 20:03:56 -0800

    added integration tests for meteor user

--- test/integration/default/serverspec/localhost/ubuntu_spec.rb
+++ test/integration/default/serverspec/localhost/ubuntu_spec.rb
@@ -23,4 +23,16 @@ describe 'prime8base::ubuntu' do
     end
   end
 
+  describe user('meteor')
+    it { should exist }
+  end
+
+  describe command('sudo -u meteor whoami')
+    its(:stdout) { should match /meteor/ }
+  end
+ 
+  describe command('sudo -u meteor meteor')
+    its(:stdout) { should match /Welcome/ }
+  end
+
 end
