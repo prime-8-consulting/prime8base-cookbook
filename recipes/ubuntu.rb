b51c4cc271e29708fc404cd643ad5f02413e19fd
munjeli <elemunjeli@gmail.com>
2016-03-08 18:55:41 -0800

    fixed graphicsmagick install

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -2,19 +2,20 @@
 # for prime 8 consulting dev
 
 prime_packages = [
- 	'wget',
-	'git',
-	'vim',
-	'tmux',
+  'vim',
+  'tmux',
+  'wget',
   'curl',
+	'git',
 	'jq',
 	'htop',
-  'graphicsmagick'
 ]
 
-package prime_packages do
+package prime_packages
+
+
+package 'graphicsmagick' do
+  options '--no-install-recommends'
   ignore_failure true
 end
-
-
 
